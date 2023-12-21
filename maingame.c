c95d5dc35d082aadcb4f3b4c4a87aea993b753b5
Fernando Julio Vunge <developervunge@gmail.com>
2023-12-21 16:11:11 +0000

    oi

--- maingame.c
+++ maingame.c
@@ -2,7 +2,20 @@
 
 int main(int argc, char const *argv[])
 {
-    ft_init_game();
-    printf("logout game\n");
+    int menu_option;
+
+    menu_option = ft_init_game();
+    if (menu_option == 0)
+    {
+        printf("Starting the game");
+    }
+    else if (menu_option == 1)
+    {
+        printf("Showing the settings");
+    }
+    else{
+        printf("logout the game");
+        return (0);
+    }
     return (0);
 }
